e42211be4044d372a337b0fa688e4ba9435c168b
digikar99 <shubhamkarayare@gmail.com>
2022-12-12 16:45:35 +0530

    [fix] make config derivation more robust across python versions

--- src/config.lisp
+++ src/config.lisp
@@ -19,21 +19,46 @@
   #P"/usr/lib/python3.8/config-3.8-x86_64-linux-gnu/libpython3.8.so")
 
 (defun return-value-as-list (cmd)
-  (mapcar (lambda (str)
-            (subseq str 2))
-          (remove ""
-                  (uiop:split-string
-                   (string-trim '(#\newline)
-                                (uiop:run-program cmd
-                                                  :output :string
-                                                  :error-output *error-output*)))
-                  :test #'string=)))
+  (remove ""
+          (uiop:split-string
+           (string-trim '(#\newline)
+                        (uiop:run-program cmd
+                                          :output :string
+                                          :error-output *error-output*)))
+          :test #'string=))
 
-(defvar *python-include-path* (pathname (nth 0 (return-value-as-list "python3-config --includes"))))
-(destructuring-bind (libpython-path more-libs-path libpython &rest more-libs)
-    (return-value-as-list "python3-config --embed --ldflags")
-  (defvar *python-shared-object-path*
-    (pathname (uiop:strcat libpython-path "/lib" libpython ".so")))
-  (defvar *python-additional-libraries* more-libs)
-  (defvar *python-additional-libraries-search-path* (pathname more-libs-path)))
+(defun strip-i-and-l (arg)
+  (declare (type string arg))
+  (cond ((< (length arg) 2)
+         arg)
+        ((member (subseq arg 0 2)
+                 '("-i" "-l")
+                 :test #'string-equal)
+         (subseq arg 2))
+        (t
+         arg)))
+
+(let ((python-version-string (second (return-value-as-list "python3 --version"))))
+  (if (uiop:version< python-version-string "3.8.0")
+      (destructuring-bind (libpython-path more-libs-path &rest more-libs)
+          (mapcar #'strip-i-and-l
+                  (return-value-as-list "python3-config --ldflags"))
+        (defvar *python-shared-object-path*
+          (let ((libpython (apply #'format nil "python~D.~D"
+                                  (subseq (uiop:parse-version python-version-string)
+                                          0 2))))
+            (pathname (uiop:strcat libpython-path "/lib" libpython ".so"))))
+        (defvar *python-additional-libraries* more-libs)
+        (defvar *python-additional-libraries-search-path* (pathname more-libs-path)))
+      (destructuring-bind (libpython-path more-libs-path libpython &rest more-libs)
+          (mapcar #'strip-i-and-l
+                  (return-value-as-list "python3-config --embed --ldflags"))
+        (defvar *python-shared-object-path*
+          (pathname (uiop:strcat libpython-path "/lib" libpython ".so")))
+        (defvar *python-additional-libraries* more-libs)
+        (defvar *python-additional-libraries-search-path* (pathname more-libs-path)))))
+
+(defvar *python-include-path*
+  (pathname (strip-i-and-l (nth 0 (return-value-as-list "python3-config --includes")))))
+
 
