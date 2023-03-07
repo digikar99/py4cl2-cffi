eece9a41d01b7ea929668c23c4cd51d1707d23cd
digikar99 <shubhamkarayare@gmail.com>
2023-03-07 14:10:35 +0530

    [fix] without numpy and with numpy

--- src/shared-objects.lisp
+++ src/shared-objects.lisp
@@ -13,20 +13,14 @@
 (defun compile-utils-shared-object ()
   (uiop:with-current-directory ((asdf:component-pathname (asdf:find-system "py4cl2-cffi")))
     (let ((program-string
-            (if (ignore-errors
-                 (uiop:run-program "python3 -c 'import numpy; print(numpy.__path__[0])'"
-                                   :output :string))
-                (format nil
-                        ;; /media/common-storage/miniconda3/include/python3.8
-                        "gcc -I'~A' -I'~A/core/include/numpy/' -c -Wall -Werror -fpic py4cl-utils.c && gcc -shared -o libpy4cl-utils.so py4cl-utils.o"
-                        (namestring *python-include-path*)
-                        (string-trim (list #\newline)
-                                     (uiop:run-program "python3 -c 'import numpy; print(numpy.__path__[0])'"
-                                                       :output :string :error-output *error-output*)))
-                (format nil
-                        ;; /media/common-storage/miniconda3/include/python3.8
-                        "gcc -I'~A' -c -Wall -Werror -fpic py4cl-utils.c && gcc -shared -o libpy4cl-utils.so py4cl-utils.o"
-                        (namestring *python-include-path*)))))
+            (format nil
+                    ;; /media/common-storage/miniconda3/include/python3.8
+                    "gcc -I'~A' -I'~Anumpy/' -c -Wall -Werror -fpic py4cl-utils.c && gcc -shared -o libpy4cl-utils.so py4cl-utils.o"
+                    (namestring *python-include-path*)
+                    (string-trim (list #\newline)
+                                 (namestring
+                                  (asdf:component-pathname
+                                   (asdf:find-system "py4cl2-cffi")))))))
       (write-line program-string)
       (uiop:run-program program-string
                         :error-output *error-output*
