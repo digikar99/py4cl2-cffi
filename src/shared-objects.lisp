d4267ffe27411b7579691971809b28bb70a0ee6c
digikar99 <shubhamkarayare@gmail.com>
2023-04-17 11:25:52 +0530

    [fix] set *numpy-installed-p* from a different place

--- src/shared-objects.lisp
+++ src/shared-objects.lisp
@@ -1,23 +1,5 @@
 (in-package :py4cl2-cffi)
 
-(eval-when (:compile-toplevel :load-toplevel)
-  (let* ((numpy-installed-p-file
-           (asdf:component-pathname
-            (asdf:find-component
-             "py4cl2-cffi" "numpy-installed-p")))
-         (numpy-installed-p-old
-           (read-file-into-string numpy-installed-p-file))
-         (numpy-installed-p-new
-           (format nil "CL:~A" (zerop (nth-value 2
-                                                 (uiop:run-program
-                                                  "python3 -c 'import numpy'"
-                                                  :ignore-error-status t))))))
-    (when (string/= numpy-installed-p-old
-                    numpy-installed-p-new)
-      (write-string-into-file numpy-installed-p-new numpy-installed-p-file
-                              :if-exists :supersede
-                              :if-does-not-exist :create))))
-
 (eval-when (:compile-toplevel :load-toplevel :execute)
 
   (defvar *utils-source-file-path*
@@ -59,3 +41,24 @@
 
 (eval-when (:compile-toplevel)
   (compile-utils-shared-object))
+
+(eval-when (:compile-toplevel :load-toplevel)
+  (let* ((numpy-installed-p-file
+           (asdf:component-pathname
+            (asdf:find-component
+             "py4cl2-cffi" "numpy-installed-p")))
+         (numpy-installed-p-old
+           (read-file-into-string numpy-installed-p-file))
+         (numpy-installed-p (zerop (nth-value 2
+                                              (uiop:run-program
+                                               "python3 -c 'import numpy'"
+                                               :ignore-error-status t))))
+         (numpy-installed-p-new
+           (format nil "CL:~A" numpy-installed-p)))
+    (when (string/= numpy-installed-p-old
+                    numpy-installed-p-new)
+      (write-string-into-file numpy-installed-p-new numpy-installed-p-file
+                              :if-exists :supersede
+                              :if-does-not-exist :create))
+    (setq *numpy-installed-p* numpy-installed-p)))
+
