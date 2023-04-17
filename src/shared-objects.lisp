fcec60e898136a02ec7c69301a3cb2929a43cc45
digikar99 <shubhamkarayare@gmail.com>
2023-04-17 11:10:19 +0530

    [fix] define several required things during compile time

--- src/shared-objects.lisp
+++ src/shared-objects.lisp
@@ -1,17 +1,5 @@
 (in-package :py4cl2-cffi)
 
-(defvar *utils-source-file-path*
-  (merge-pathnames
-   (asdf:component-pathname (asdf:find-system "py4cl2-cffi"))
-   #p"py4cl-utils.c"))
-
-(defvar *utils-shared-object-path*
-  (merge-pathnames
-   (asdf:component-pathname (asdf:find-system "py4cl2-cffi"))
-   #p"libpy4cl-utils.so"))
-
-(defvar *numpy-installed-p*)
-
 (eval-when (:compile-toplevel :load-toplevel)
   (let* ((numpy-installed-p-file
            (asdf:component-pathname
@@ -30,30 +18,44 @@
                               :if-exists :supersede
                               :if-does-not-exist :create))))
 
-(defun compile-utils-shared-object ()
-  (uiop:with-current-directory ((asdf:component-pathname (asdf:find-system "py4cl2-cffi")))
-    (multiple-value-bind (numpy-path error-output error-status)
-        (uiop:run-program
-         "cd ~/; python3 -c 'import numpy; print(numpy.__path__[0])'"
-         :output :string :ignore-error-status t)
-      (declare (ignore error-output))
-      (let* ((numpy-installed-p
-               (zerop error-status))
-             (program-string
-               (format nil
-                       "gcc -I'~A' -I'~A' -c -Wall -Werror -fpic py4cl-utils.c && gcc -shared -o libpy4cl-utils.so py4cl-utils.o"
-                       (namestring *python-include-path*)
-                       (if numpy-installed-p
-                           (format nil "~A/core/include/"
-                                   (string-trim (list #\newline) numpy-path))
-                           (namestring
-                            (asdf:component-pathname
-                             (asdf:find-system "py4cl2-cffi")))))))
-        (setq *numpy-installed-p* numpy-installed-p)
-        (write-line program-string)
-        (uiop:run-program program-string
-                          :error-output *error-output*
-                          :output *standard-output*)))))
+(eval-when (:compile-toplevel :load-toplevel :execute)
+
+  (defvar *utils-source-file-path*
+    (merge-pathnames
+     (asdf:component-pathname (asdf:find-system "py4cl2-cffi"))
+     #p"py4cl-utils.c"))
+
+  (defvar *utils-shared-object-path*
+    (merge-pathnames
+     (asdf:component-pathname (asdf:find-system "py4cl2-cffi"))
+     #p"libpy4cl-utils.so"))
+
+  (defvar *numpy-installed-p*)
+
+  (defun compile-utils-shared-object ()
+    (uiop:with-current-directory ((asdf:component-pathname (asdf:find-system "py4cl2-cffi")))
+      (multiple-value-bind (numpy-path error-output error-status)
+          (uiop:run-program
+           "cd ~/; python3 -c 'import numpy; print(numpy.__path__[0])'"
+           :output :string :ignore-error-status t)
+        (declare (ignore error-output))
+        (let* ((numpy-installed-p
+                 (zerop error-status))
+               (program-string
+                 (format nil
+                         "gcc -I'~A' -I'~A' -c -Wall -Werror -fpic py4cl-utils.c && gcc -shared -o libpy4cl-utils.so py4cl-utils.o"
+                         (namestring *python-include-path*)
+                         (if numpy-installed-p
+                             (format nil "~A/core/include/"
+                                     (string-trim (list #\newline) numpy-path))
+                             (namestring
+                              (asdf:component-pathname
+                               (asdf:find-system "py4cl2-cffi")))))))
+          (setq *numpy-installed-p* numpy-installed-p)
+          (write-line program-string)
+          (uiop:run-program program-string
+                            :error-output *error-output*
+                            :output *standard-output*))))))
 
 (eval-when (:compile-toplevel)
   (compile-utils-shared-object))
