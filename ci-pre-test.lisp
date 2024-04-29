c8ec8c1340d8aef38d6775731b088d1a52dd9312
digikar <shubhamkarayare@gmail.com>
2024-04-30 00:42:19 +0530

    [ci] more step-by-step ci-pre-test

--- ci-pre-test.lisp
+++ ci-pre-test.lisp
@@ -11,5 +11,18 @@
 (ql:quickload "trivial-backtrace")
 (format t "~%TRIVIAL-BACKTRACE loaded.~%")
 (force-output)
-(ql:quickload "py4cl2-cffi-tests")
+(defmacro print-and-call (form)
+  `(progn
+     (format t "~%Executing ~S~%" ',form)
+     ,form
+     (format t "~%DONE Executing ~S~%" ',form)))
+(print-and-call
+ (ql:quickload "py4cl2-cffi"))
+(print-and-call
+ (py4cl2-cffi:pystart))
+(print-and-call
+ (py4cl2-cffi:import-module "math"))
+(print-and-call
+ (py4cl2-cffi:defpymodule "math"))
+;; (ql:quickload "py4cl2-cffi-tests")
 (uiop:quit 0)
