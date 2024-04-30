1e9569707fd7eaf5d18ef1bc0b03478c69cad99c
digikar <shubhamkarayare@gmail.com>
2024-04-30 10:11:43 +0530

    [ci] use print-and-call

--- ci-pre-test.lisp
+++ ci-pre-test.lisp
@@ -11,5 +11,11 @@
      (format t "~%Executing ~S~%" ',form)
      ,form
      (format t "~%DONE Executing ~S~%" ',form)))
-(ql:quickload "py4cl2-cffi-tests")
+(print-and-call (ql:quickload "py4cl2-cffi"))
+(print-and-call (py4cl2-cffi:pystart))
+(print-and-call (py4cl2-cffi:pystart))
+(print-and-call (py4cl2-cffi:import-module "math"))
+(print-and-call
+ (py4cl2-cffi:defpymodule "math" nil :continue-ignoring-errors nil))
+;; (ql:quickload "py4cl2-cffi-tests")
 (uiop:quit 0)
