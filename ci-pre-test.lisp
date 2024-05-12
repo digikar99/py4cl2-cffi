e5a2d64e31030c6901a2446c9e93ab200e09a426
digikar <shubhamkarayare@gmail.com>
2024-05-12 22:32:50 +0530

    [ci] push :ci instead of :travis

--- ci-pre-test.lisp
+++ ci-pre-test.lisp
@@ -2,7 +2,8 @@
 ;; (ql-dist:install-dist "http://beta.quicklisp.org/dist/quicklisp/2020-10-16/distinfo.txt"
 ;;                       :replace t
 ;;                       :prompt nil)
-(push :travis *features*)
+(push :ci *features*)
+(print *features*)
 (push #P"./" ql:*local-project-directories*)
 (print (ql:where-is-system "py4cl2-cffi"))
 (ql:quickload "py4cl2-cffi-tests")
