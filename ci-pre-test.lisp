31baba16ab20f1bd4281307631bff9ac948d7ea6
digikar <shubhamkarayare@gmail.com>
2024-05-29 00:35:30 +0530

    [ci] download-dependencies in ci-pre-test instead of github actions

--- ci-pre-test.lisp
+++ ci-pre-test.lisp
@@ -6,5 +6,12 @@
 (print *features*)
 (push #P"./" ql:*local-project-directories*)
 (print (ql:where-is-system "py4cl2-cffi"))
+
+(ql:quickload "download-dependencies")
+(in-package :download-dependencies)
+(let ((*dependencies-home* (first ql:*local-project-directories*)))
+  (ensure-system "py4cl2-cffi-tests"))
+
+(in-package :cl-user)
 (ql:quickload "py4cl2-cffi-tests")
 (uiop:quit 0)
