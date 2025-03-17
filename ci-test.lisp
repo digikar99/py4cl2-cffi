12297c36569d79f6c8a7c85cce22f222efd7e733
matzmueller <martin.atzmueller@uni-osnabrueck.de>
2025-03-17 12:50:19 +0100

    [fix] use eval-when in ci-test

--- ci-test.lisp
+++ ci-test.lisp
@@ -1,30 +1,31 @@
 (in-package :cl-user)
 
+(eval-when (:compile-toplevel :load-toplevel :execute)
+  (ql:quickload "py4cl2-cffi-tests"))
+
 (push :ci *features*)
 (when (eq :linux (uiop:operating-system))
   (push (print (pathname (uiop:getenv "EXOTIC_DIR")))
         ql:*local-project-directories*))
 (push #P"./" ql:*local-project-directories*)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (progn
-    (ql:quickload "py4cl2-cffi-tests")
+(progn
+  (ql:quickload "py4cl2-cffi-tests")
     
-    (py4cl2-cffi/config:print-configuration)
+  (py4cl2-cffi/config:print-configuration)
     
-    (let ((report (py4cl2-cffi-tests:run)))
-      (when (or (plusp (slot-value report 'clunit::failed))
-		(plusp (slot-value report 'clunit::errors)))
-	(uiop:quit 1)))
-    (terpri)
-    (format t "!!MULTI-THREADED TESTS RAN SUCCESSFULLY!!"))
-  
+  (let ((report (py4cl2-cffi-tests:run)))
+    (when (or (plusp (slot-value report 'clunit::failed))
+	      (plusp (slot-value report 'clunit::errors)))
+      (uiop:quit 1)))
+  (terpri)
+  (format t "!!MULTI-THREADED TESTS RAN SUCCESSFULLY!!"))
 
-  #-:ccl
-  (progn
-    (ql:quickload "py4cl2-cffi/single-threaded")
-    (py4cl2-cffi/single-threaded:pystart)
-    (assert (string= "42" (py4cl2-cffi/single-threaded:pycall "str" 42)))
-    (terpri)
-    (format t "!!Preliminary SINGLE-THREADED TESTS RAN SUCCESSFULLY!!")
-    (uiop:quit 0)))
+
+(progn
+  (ql:quickload "py4cl2-cffi/single-threaded")
+  (py4cl2-cffi/single-threaded:pystart)
+  (assert (string= "42" (py4cl2-cffi/single-threaded:pycall "str" 42)))
+  (terpri)
+  (format t "!!Preliminary SINGLE-THREADED TESTS RAN SUCCESSFULLY!!")
+  (uiop:quit 0))
