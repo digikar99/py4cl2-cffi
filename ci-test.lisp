40761e58b128ac0a580a6623f651f615adc87197
matzmueller <martin.atzmueller@uni-osnabrueck.de>
2025-03-17 12:55:56 +0100

    [fix] simplify

--- ci-test.lisp
+++ ci-test.lisp
@@ -1,27 +1,23 @@
 (in-package :cl-user)
 
-(eval-when (:compile-toplevel :load-toplevel :execute)
-  (ql:quickload "py4cl2-cffi-tests"))
-
 (push :ci *features*)
 (when (eq :linux (uiop:operating-system))
   (push (print (pathname (uiop:getenv "EXOTIC_DIR")))
         ql:*local-project-directories*))
 (push #P"./" ql:*local-project-directories*)
 
-(progn
-  (ql:quickload "py4cl2-cffi-tests")
+(ql:quickload "py4cl2-cffi-tests")
     
-  (py4cl2-cffi/config:print-configuration)
+(py4cl2-cffi/config:print-configuration)
     
-  (let ((report (py4cl2-cffi-tests:run)))
-    (when (or (plusp (slot-value report 'clunit::failed))
-	      (plusp (slot-value report 'clunit::errors)))
-      (uiop:quit 1)))
-  (terpri)
-  (format t "!!MULTI-THREADED TESTS RAN SUCCESSFULLY!!"))
+(let ((report (py4cl2-cffi-tests:run)))
+  (when (or (plusp (slot-value report 'clunit::failed))
+	    (plusp (slot-value report 'clunit::errors)))
+    (uiop:quit 1)))
+(terpri)
+(format t "!!MULTI-THREADED TESTS RAN SUCCESSFULLY!!")
 
-
+#-:ccl
 (progn
   (ql:quickload "py4cl2-cffi/single-threaded")
   (py4cl2-cffi/single-threaded:pystart)
